6cfe728852c6025ebf181c59fd1087ada49fbcb8
Esther White <monacodelisa@gmail.com>
2024-02-08 15:44:26 +0200

    now

--- angular-v17-theme-angular-material/src/app/components/header/header.component.ts
+++ angular-v17-theme-angular-material/src/app/components/header/header.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { Component, inject } from "@angular/core";
+import { ThemeService } from "../../services/theme.service";
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+	selector: "app-header",
+	standalone: true,
+	imports: [
+		MatToolbarModule,
+		MatButtonModule,
+		MatSlideToggleModule,
+		MatIconModule,
+	],
+	templateUrl: "./header.component.html",
+	styleUrl: "./header.component.scss",
 })
 export class HeaderComponent {
+	themeService: ThemeService = inject(ThemeService);
+	theme = this.themeService.themeSignal();
 
+	toggleTheme() {
+		this.themeService.updateTheme();
+	}
 }
